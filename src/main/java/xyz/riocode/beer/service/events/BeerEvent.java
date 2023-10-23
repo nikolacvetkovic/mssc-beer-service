19aff4ff64b679186d1f5e9b1f01787a83997efb
Nikola <nikola.cvetkovic91@gmail.com>
2023-10-23 15:33:05 +0200

    add no args constructor for jackson

--- src/main/java/xyz/riocode/beer/service/events/BeerEvent.java
+++ src/main/java/xyz/riocode/beer/service/events/BeerEvent.java
@@ -1,21 +1,23 @@
 package xyz.riocode.beer.service.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import xyz.riocode.beer.service.web.model.BeerDto;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class BeerEvent implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -6977273018284813967L;
 
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 
 }
