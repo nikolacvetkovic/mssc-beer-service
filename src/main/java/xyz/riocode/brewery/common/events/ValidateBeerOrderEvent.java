33b849b1ec0907748d09af490eb3eb24a4c8f375
Nikola <nikola.cvetkovic91@gmail.com>
2023-11-02 10:00:07 +0100

    add lombok constructor annotation

--- src/main/java/xyz/riocode/brewery/common/events/ValidateBeerOrderEvent.java
+++ src/main/java/xyz/riocode/brewery/common/events/ValidateBeerOrderEvent.java
@@ -1,10 +1,14 @@
 package xyz.riocode.brewery.common.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import xyz.riocode.brewery.common.model.BeerOrderDto;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ValidateBeerOrderEvent {
     private BeerOrderDto beerOrderDto;
--- src/main/java/xyz/riocode/brewery/common/events/ValidateBeerOrderResultEvent.java
+++ src/main/java/xyz/riocode/brewery/common/events/ValidateBeerOrderResultEvent.java
@@ -1,11 +1,15 @@
 package xyz.riocode.brewery.common.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ValidateBeerOrderResultEvent {
     private UUID orderId;
