d50cb0a5e50d790e386964d1c8311a87829a41a2
Nikola <nikola.cvetkovic91@gmail.com>
2023-12-04 15:05:03 +0100

    configure open feign client to use http basic auth

--- src/main/java/xyz/riocode/brewery/beer/service/config/FeignClientConfig.java
+++ src/main/java/xyz/riocode/brewery/beer/service/config/FeignClientConfig.java
@@ -1,10 +1,21 @@
 package xyz.riocode.brewery.beer.service.config;
 
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import xyz.riocode.brewery.beer.service.services.inventory.BeerInventoryFailoverFeignClient;
 import xyz.riocode.brewery.beer.service.services.inventory.BeerInventoryFeignClient;
 
 @EnableFeignClients(clients = {BeerInventoryFeignClient.class, BeerInventoryFailoverFeignClient.class})
 @Configuration
-public class FeignClientConfig {}
+public class FeignClientConfig {
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(@Value("${xyz.brewery.beer-inventory-service-user}") String inventoryUser,
+                                                                   @Value("${xyz.brewery.beer-inventory-service-password}") String inventoryPassword) {
+        return new BasicAuthRequestInterceptor(inventoryUser, inventoryPassword);
+    }
+
+}
--- src/main/java/xyz/riocode/brewery/beer/service/services/inventory/BeerInventoryFeignClient.java
+++ src/main/java/xyz/riocode/brewery/beer/service/services/inventory/BeerInventoryFeignClient.java
@@ -5,12 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import xyz.riocode.brewery.beer.service.config.FeignClientConfig;
 import xyz.riocode.brewery.beer.service.services.inventory.model.BeerInventoryDto;
 
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "beer-inventory-service", fallback = BeerInventoryFailoverFeignClientImpl.class)
+@FeignClient(name = "beer-inventory-service", fallback = BeerInventoryFailoverFeignClientImpl.class,
+            configuration = FeignClientConfig.class)
 public interface BeerInventoryFeignClient {
     @RequestMapping(method = RequestMethod.GET, value = "/api/v1/beer/{beerId}/inventory")
     ResponseEntity<List<BeerInventoryDto>> getOnHandInventory(@PathVariable UUID beerId);
